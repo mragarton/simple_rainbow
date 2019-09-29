3e28d346ae5da911f0dfaa73e0d6d2fcd56f94c3
Garton <35609212+mragarton@users.noreply.github.com>
2019-09-29 17:26:38 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+let Discord = require('discord.js'), fs = require('fs'), Rainbow = require('color-rainbow'), client = new Discord.Client(), data = JSON.parse(fs.readFileSync('./cfg.json')), colors = [], update = async _ => colors = Rainbow.create(data["rainbowArraySize"]);
+client.login(data["token"]).then(_ => {
+  console.log(`${client.user.tag}: Logged in!`);
+  const guild = client.guilds.find(g => g.name.toLowerCase() === data["guildName"].toLowerCase());
+  if (!guild) return console.log('Cannot find guild. (Please check the config file!)');
+  const role = guild.roles.find(r => r.name.toLowerCase() === data["roleName"].toLowerCase());
+  if (!role) return console.log('Cannot find role. (Please check the config file!)');
+  colorize(role);
+}).catch(console.error);
+const colorize = async role => {
+  console.log(`Colorizing started! (${data["changeRate"]} seconds)`);
+  setInterval(async _ => {
+    if (colors.length === 0) await update();
+    const color = colors.pop();
+    role.setColor(color.values.rgb).catch(_ => { });
+  }, data["changeRate"] * 1000);
+};
